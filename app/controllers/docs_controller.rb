4ac1692b4a3db6f0e495b42e6cfb163b5dfb1062
Carlos Alves <carlosalves369m@hotmail.com>
2021-09-27 11:46:48 -0300

    add form haml

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -9,11 +9,17 @@ class DocsController < ApplicationController
     end
 
     def new
-        # 
+        @doc = Doc.new
     end
 
     def create
-        # 
+        @doc = Doc.new(doc_params)
+
+        if @doc.save
+            redirect_to @doc
+        else
+            render 'new'
+        end
     end
 
     def edit
@@ -34,6 +40,6 @@ class DocsController < ApplicationController
         end
 
         def doc_params
-            # 
+            params.require(:doc).permit(:title, :content)
         end
 end
